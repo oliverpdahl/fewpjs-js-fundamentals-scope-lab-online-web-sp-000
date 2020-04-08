0e454c1d1d00b4ade69073e733cd7c135137ea91
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 19:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  const animal = 'cat';
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
-
-  // Feel free to move things around!
-  two = 2
 }
